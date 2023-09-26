c83c5f6ad0318c8a912d80516073687873d7fd97
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-09-27 04:08:37 +0530

    removed AuthTutorial folder and files

--- AuthTutorial/Api/auth.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-SECRET_KEY: str = "8a30f931e68f0606639b27c2420cc5e6a7cd6f62ca0338ff2d943cd2174d51ad"
-ALGORITHM: str  = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
