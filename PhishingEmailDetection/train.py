e49372469a5c7afa9b50e736651657019b4562f0
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-10-02 20:41:55 +0530

    fixes

--- PhishingEmailDetection/train.py
+++ PhishingEmailDetection/train.py
@@ -54,14 +54,96 @@ def main() -> None:
     features = vectorizer.fit_transform(raw.clean.values)
 
     train_inputs, test_inputs, train_target, test_target = split(
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         features, raw.Class.values, test_size=.30, stratify=raw.Class.values, random_state=42)
+||||||| Stash base
+        features,
+        raw.Class.values,
+        test_size=.30,
+        stratify=raw.Class.values,
+        random_state=42)
+=======
+        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
+>>>>>>> Stashed changes
+||||||| Stash base
+        features,
+        raw.Class.values,
+        test_size=.30,
+        stratify=raw.Class.values,
+        random_state=42)
+=======
+        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
+>>>>>>> Stashed changes
+||||||| Stash base
+        features,
+        raw.Class.values,
+        test_size=.30,
+        stratify=raw.Class.values,
+        random_state=42)
+=======
+        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
+>>>>>>> Stashed changes
 
     model = Model(train_inputs, train_target)
     model.fit()
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     filename = "email_phishing_detection.0.1.0.joblib"
     # model.save_model(filename=filename)
 
+||||||| Stash base
+    ex = [
+        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
+        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
+        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
+        is actively hiring"""
+    ]
+
+    ex_feat = vectorizer.transform(ex)
+||||||| Stash base
+    ex = [
+        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
+        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
+        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
+        is actively hiring"""
+    ]
+
+    ex_feat = vectorizer.transform(ex)
+=======
+>>>>>>> Stashed changes
+||||||| Stash base
+    ex = [
+        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
+        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
+        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
+        is actively hiring"""
+    ]
+
+    ex_feat = vectorizer.transform(ex)
+=======
+>>>>>>> Stashed changes
+    filename = "phishing.joblib"
+<<<<<<< Updated upstream
+    model.save_model(filename=filename)
+
+=======
+    filename = "phishing.joblib"
+    model.save_model(filename=filename)
+||||||| Stash base
+    # model.save_model(filename=filename)
+
+    m2, _ = joblib.load(filename=filename)
+    ex_pred = m2.predict(ex_feat)
+    print(ex_pred)
+=======
+    model.save_model(filename=filename)
+>>>>>>> Stashed changes
+
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     main()
