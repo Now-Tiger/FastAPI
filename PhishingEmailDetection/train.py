d4bd1fd5b6b9336c13865e289251183ce7586dff
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-10-09 03:03:05 +0530

    "updated files"

--- PhishingEmailDetection/train.py
+++ PhishingEmailDetection/train.py
@@ -53,129 +53,13 @@ def main() -> None:
     vectorizer = TfidfVectorizer(norm='l2', decode_error='ignore', binary=True)
     features = vectorizer.fit_transform(raw.clean.values)
 
-    train_inputs, test_inputs, train_target, test_target = split(
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        features, raw.Class.values, test_size=.30, stratify=raw.Class.values, random_state=42)
-||||||| Stash base
-        features,
-        raw.Class.values,
-        test_size=.30,
-        stratify=raw.Class.values,
-        random_state=42)
-=======
-        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
->>>>>>> Stashed changes
-||||||| Stash base
-        features,
-        raw.Class.values,
-        test_size=.30,
-        stratify=raw.Class.values,
-        random_state=42)
-=======
-        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
->>>>>>> Stashed changes
-||||||| Stash base
-        features,
-        raw.Class.values,
-        test_size=.30,
-        stratify=raw.Class.values,
-        random_state=42)
-=======
-        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
->>>>>>> Stashed changes
-||||||| Stash base
-        features,
-        raw.Class.values,
-        test_size=.30,
-        stratify=raw.Class.values,
-        random_state=42)
-=======
-        features, raw.Class.values, test_size=.30, stratify=raw.Class.values,random_state=42)
->>>>>>> Stashed changes
-
+    train_inputs, _, train_target, _ = split(
+        features, raw.Class.values, stratify=raw.Class, random_state=42, test_size=.30)
+    
     model = Model(train_inputs, train_target)
-    model.fit()
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    filename = "email_phishing_detection.0.1.0.joblib"
-    # model.save_model(filename=filename)
-
-||||||| Stash base
-    ex = [
-        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
-        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
-        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
-        is actively hiring"""
-    ]
-
-    ex_feat = vectorizer.transform(ex)
-||||||| Stash base
-    ex = [
-        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
-        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
-        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
-        is actively hiring"""
-    ]
-
-    ex_feat = vectorizer.transform(ex)
-=======
->>>>>>> Stashed changes
-||||||| Stash base
-    ex = [
-        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
-        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
-        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
-        is actively hiring"""
-    ]
-
-    ex_feat = vectorizer.transform(ex)
-=======
->>>>>>> Stashed changes
-||||||| Stash base
-    ex = [
-        """swapnil.narwade3@gmail.com Job For You <vacancy@job.shine.com> 
-        Do Not forward this mail, it contains links which allow direct login to your Shine account. 
-        Emailer Dear Swapnil, Recruiter from Mercedes Benz Research And Development India Pvt.Ltd. 
-        is actively hiring"""
-    ]
-
-    ex_feat = vectorizer.transform(ex)
-=======
->>>>>>> Stashed changes
-    filename = "phishing.joblib"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    model.save_model(filename=filename)
-
-=======
     filename = "phishing.joblib"
     model.save_model(filename=filename)
-||||||| Stash base
-    # model.save_model(filename=filename)
 
-    m2, _ = joblib.load(filename=filename)
-    ex_pred = m2.predict(ex_feat)
-    print(ex_pred)
-=======
-    model.save_model(filename=filename)
->>>>>>> Stashed changes
-||||||| Stash base
-    # model.save_model(filename=filename)
-
-    m2, _ = joblib.load(filename=filename)
-    ex_pred = m2.predict(ex_feat)
-    print(ex_pred)
-=======
-    model.save_model(filename=filename)
->>>>>>> Stashed changes
-
->>>>>>> Stashed changes
 
 if __name__ == "__main__":
     main()
